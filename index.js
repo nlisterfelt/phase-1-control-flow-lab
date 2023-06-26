f701858ba4f615cdf11ed22c5e08b011b1125217
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-06-26 15:22:40 -0500

    work with if, ternary and switch

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(someValue){
+  let result;
+  if(someValue <= 400){
+    result = 'This one is on me!'
+  } else if(someValue <= 2000){
+    result = 'That will be twenty bucks.'
+  } else if(someValue <= 2500){
+    result = 'I will gladly take your thirty bucks.'
+  } else {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response;
+  city === 'NYC' ? (response = 'Ok, sounds good.') : (response = 'No go.')
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message
+  switch (tip){
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.'
+      break;
+  }
+  return message;
 }
